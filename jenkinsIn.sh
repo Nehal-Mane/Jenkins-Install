e4ea8d76dca44d6ece93529017d3d326b574e95e
Nehal-Mane <159645796+MaxDev-5151@users.noreply.github.com>
2024-04-20 09:04:21 +0530

    Create jenkinsIn.sh

---
+++ jenkinsIn.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install openjdk-11-jdk
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo systemctl enable jenkins
